aa9295b40f6df154bcb67b330dda442ddbe6a291
errorb0t <foelschd@gmail.com>
2023-08-26 09:41:39 +0200

    Import cleanup, naming guidelines, specific errors

--- cogs/game_cog.py
+++ cogs/game_cog.py
@@ -1,55 +1,65 @@
 import discord
-from discord import ApplicationContext, Option, option
-import commands.game as game
-from exceptions import *
-from typing import List, Dict, Any
+from discord import (  # pylint: disable = no-name-in-module
+    ApplicationContext,
+    option,
+    slash_command,
+)
 from discord.ext import commands
-from env_load import *
+
+import commands.game as game
+from exceptions import (
+    CouldNotFindChannelException,
+    GameInProgressException,
+    NoGameInProgressException,
+    NoGuildException,
+    NotAdminException,
+    NotEnoughPlayersException,
+)
 
 
 class GameCog(commands.Cog):
     def __init__(self, bot):
         self.bot: discord.Bot = bot
 
-    def convert_team_to_string(self, team: Dict[str, game.Player]) -> str:
+    def convert_team_to_string(self, team: dict[str, game.Player]) -> str:
         return_str = ""
         for role in ["tank", "support", "assassin", "assassin2", "offlane"]:
             return_str += f"<@{team[role].user.id}>\n\n"
         return return_str
 
-    @discord.slash_command(name="start", description="Start a game")
+    @slash_command(name="start", description="Start a game")
     async def slash_start_game(self, ctx: ApplicationContext):
         try:
             current_game = await game.start_game(ctx)
             if current_game:
                 current_game = game.CurrentGame()
                 print(current_game)
-                map: str = current_game.map
-                team1: Dict[str, game.Player] = current_game.team_1
-                team2: Dict[str, game.Player] = current_game.team_2
-                fp = current_game.first_pick
+                current_map: str = current_game.map
+                team1: dict[str, game.Player] = current_game.team_1
+                team2: dict[str, game.Player] = current_game.team_2
+                first_pick = current_game.first_pick
 
-            banner = f"https://static.icy-veins.com/images/heroes/tier-lists/maps/{map.replace(' ', '-').lower()}.jpg"
+            banner = f"https://static.icy-veins.com/images/heroes/tier-lists/maps/{current_map.replace(' ', '-').lower()}.jpg"
             embed = discord.Embed(
                 title="**Game Started**",
                 color=discord.Colour.blurple(),
-                description=map,
+                description=current_map,
             )
             embed.set_author(
                 name=f"Started by {ctx.user.display_name}",
                 icon_url=ctx.user.display_avatar,
             )
             embed.add_field(
-                name=f"Team 1" + (" (First Pick)" if fp == 1 else ""),
+                name="Team 1" + (" (First Pick)" if first_pick == 1 else ""),
                 value=self.convert_team_to_string(team1),
                 inline=True,
             )
             embed.add_field(
                 name="VS.",
-                value=f"TANK\n\nSUPPORT\n\nASSASSIN\n\nASSASSIN\n\nOFFLANE",
+                value="TANK\n\nSUPPORT\n\nASSASSIN\n\nASSASSIN\n\nOFFLANE",
             )
             embed.add_field(
-                name=f"Team 2" + (" (First Pick)" if fp == 2 else ""),
+                name="Team 2" + (" (First Pick)" if first_pick == 2 else ""),
                 value=self.convert_team_to_string(team2),
                 inline=True,
             )
@@ -62,7 +72,7 @@ class GameCog(commands.Cog):
                 description="Not enough players to start a game!",
             )
             await ctx.respond(embed=embed, ephemeral=True)
-        except GameIsInProgressException:
+        except GameInProgressException:
             embed = discord.Embed(
                 title="Error",
                 color=discord.Colour.red(),
@@ -77,7 +87,7 @@ class GameCog(commands.Cog):
             )
             await ctx.respond(embed=embed, ephemeral=True)
 
-    @discord.slash_command(name="end", description="End a currently running game")
+    @slash_command(name="end", description="End a currently running game")
     @option(
         "winner",
         str,
@@ -91,7 +101,7 @@ class GameCog(commands.Cog):
             # Move everyone back to the lobby.
             try:
                 await game.move_all_team_players_to_lobby(ctx)
-            except:
+            except (NoGuildException, CouldNotFindChannelException):
                 pass
             embed = discord.Embed(
                 title="Game Ended",
@@ -114,13 +124,13 @@ class GameCog(commands.Cog):
             )
             await ctx.respond(embed=embed, ephemeral=True)
 
-    @discord.slash_command(name="cancel", description="Cancel a currently running game")
+    @slash_command(name="cancel", description="Cancel a currently running game")
     async def slash_cancel_game(self, ctx: ApplicationContext):
         try:
             game.cancel_game(ctx)
             try:
                 await game.move_all_team_players_to_lobby(ctx)
-            except:
+            except (NoGuildException, CouldNotFindChannelException):
                 pass
             embed = discord.Embed(
                 title="Game Cancelled",
